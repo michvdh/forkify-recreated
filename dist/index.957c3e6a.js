c9d606a92b7bcd33d88ea83284a6ff99157675bd
michvdh <michael.v.d.hingpit@gmail.com>
2022-04-25 15:16:27 +0800

    removed dist from gitignore

---
+++ dist/index.957c3e6a.js
@@ -0,0 +1,12 @@
+/* Bookmarks preview
+1. show bookmarks preview when hovering on bookmarks button
+*/ const bookmarksList = document.querySelector('.btn--bookmarks');
+console.log(bookmarksList); /* Search result
+1. container will close/hide if a search item is clicked
+2. container will show always on the side once screen size reaches a certain point
+*/  /* Upload
+1. show upload form when upload button is clicked
+2. when submit button is clicked, clear form
+*/ 
+
+//# sourceMappingURL=index.957c3e6a.js.map
