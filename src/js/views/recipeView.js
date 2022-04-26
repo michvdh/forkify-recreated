df0316b09c037474c84a7590d3b2f1d6e40d5bf2
michvdh <michael.v.d.hingpit@gmail.com>
2022-04-26 15:26:53 +0800

    changed fractional to fracty in recipeView.js

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,6 @@
 import icons from 'url:../../img/icons.svg';
 import { Fraction } from 'fractional';
+import fracty from "fracty";
 import View from './View.js';
 
 class RecipeView extends View {
@@ -89,7 +90,7 @@ class RecipeView extends View {
       <svg class="icon icon--accent icon--small">
         <use href="${icons}#icon-check"></use>
       </svg>
-      <span class="text">${ing.quantity ? new Fraction(ing.quantity).toString() : ''} ${ing.unit ? ing.unit : ''} ${ing.description}</span>
+      <span class="text">${ing.quantity ? fracty(ing.quantity).toString() : ''} ${ing.unit ? ing.unit : ''} ${ing.description}</span>
     </li>
     `;
   } 
